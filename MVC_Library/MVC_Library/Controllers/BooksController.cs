c6c2d796624e07ea2007a00d9da64175a5386f8a
Marko <todic.marko@outlook.com>
2020-02-18 12:10:04 +0100

    Index and Details views for Books and Members

--- MVC_Library/MVC_Library/Controllers/BooksController.cs
+++ MVC_Library/MVC_Library/Controllers/BooksController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using MVC_Library.Models;
 
 namespace MVC_Library.Controllers
 {
     public class BooksController : Controller
     {
-        // GET: Books
-        public ActionResult Index()
+        private ApplicationDbContext _context;
+        public BooksController()
         {
-            return View();
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Books
+        public ViewResult Index()
+        {
+            var books = _context.Books.Include(b => b.Genre).ToList();
+            return View(books);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var book = _context.Books.Include(b => b.Genre).SingleOrDefault(b => b.ID == id);
+            if( book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
         }
     }
 }
--- MVC_Library/MVC_Library/Controllers/MembersController.cs
+++ MVC_Library/MVC_Library/Controllers/MembersController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using MVC_Library.Models;
 
 namespace MVC_Library.Controllers
 {
     public class MembersController : Controller
     {
-        // GET: Members
-        public ActionResult Index()
+        private ApplicationDbContext _context;
+        public MembersController()
         {
-            return View();
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Members
+        public ViewResult Index()
+        {
+            var members = _context.Members.Include(m => m.MembershipType).ToList();
+            return View(members);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var member = _context.Members.Include(m => m.MembershipType).SingleOrDefault(m => m.ID == id);
+            if(member == null)
+            {
+                return HttpNotFound();
+            }
+            return View(member);
         }
     }
 }
