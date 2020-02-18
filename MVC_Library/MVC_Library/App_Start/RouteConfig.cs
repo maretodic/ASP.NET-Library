459b14e3d4b71a7c221a09e72b82c10f6530a532
Marko <todic.marko@outlook.com>
2020-02-18 16:11:53 +0100

    Added camelcase and routing

--- MVC_Library/MVC_Library/App_Start/RouteConfig.cs
+++ MVC_Library/MVC_Library/App_Start/RouteConfig.cs
@@ -13,6 +13,7 @@ namespace MVC_Library
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes();
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
