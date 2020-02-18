9de789576db4499b0f3b52275329b74f964c78da
Marko <todic.marko@outlook.com>
2020-02-18 16:41:05 +0100

    Prevent anonymous access

--- MVC_Library/MVC_Library/App_Start/FilterConfig.cs
+++ MVC_Library/MVC_Library/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace MVC_Library
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
         }
     }
 }
