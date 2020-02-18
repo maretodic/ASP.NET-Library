b59c18d7c1658ce9452a6914ec6a48083d0f9bd8
Marko <todic.marko@outlook.com>
2020-02-18 10:55:07 +0100

    Initail commit

---
+++ MVC_Library/MVC_Library/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Library.Startup))]
+namespace MVC_Library
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
