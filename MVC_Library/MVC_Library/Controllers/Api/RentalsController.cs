7872f8606892dc654de6a6190c8ee2ea28fbb179
Marko <todic.marko@outlook.com>
2020-02-19 11:21:35 +0100

    Added View All rentals

---
+++ MVC_Library/MVC_Library/Controllers/Api/RentalsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.Entity;
+using AutoMapper;
+using MVC_Library.Dto;
+using MVC_Library.Models;
+
+namespace MVC_Library.Controllers.Api
+{
+    public class RentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //GET /api/rentals
+        [HttpGet]
+        public IHttpActionResult GetRentals()
+        {
+            var rentalsQuery = _context.Rentals.Include(r => r.Member).Include(r => r.Book);
+            var rentalsDto = rentalsQuery.ToList().Select(Mapper.Map<Rental, RentalDto>);
+
+            return Ok(rentalsDto);
+        }
+
+        //GET /api/rentals/1
+        [HttpGet]
+        public IHttpActionResult GetRental(int id)
+        {
+            var rental = _context.Rentals.SingleOrDefault(r => r.ID == id);
+            if(rental == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Mapper.Map<Rental, RentalDto>(rental));
+        }
+    }
+}
--- MVC_Library/MVC_Library/Controllers/RentalsController.cs
+++ MVC_Library/MVC_Library/Controllers/RentalsController.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_Library.Models;
+using System.Data.Entity;
 
 namespace MVC_Library.Controllers
 {
     public class RentalsController : Controller
     {
+        private ApplicationDbContext _context;
+        public RentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Rentals
         public ActionResult New()
         {
             return View();
+        }
+
+        public ActionResult GetRentals()
+        {
+            return View("List");
+        }
+
+        public void ReturnRental(int id)
+        {
+            var rental = _context.Rentals.Include(r => r.Member).Include(r => r.Book).SingleOrDefault(r => r.ID == id);
+
+            rental.DateReturned = DateTime.Now;
+            rental.Book.NumberAvailable++;
+            _context.SaveChanges();
         }
     }
 }
