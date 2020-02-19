138f78996ade2c9aaf73a9b2f322d396f24ed991
Marko <todic.marko@outlook.com>
2020-02-19 11:03:22 +0100

    Created RentalDto

---
+++ MVC_Library/MVC_Library/Dto/RentalDto.cs
@@ -0,0 +1,18 @@
+﻿using MVC_Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Library.Dto
+{
+    public class RentalDto
+    {
+        public int ID { get; set; }
+
+        public Member Member { get; set; }
+        public Book Book { get; set; }
+        public DateTime DateRented { get; set; }
+        public DateTime? DateReturned { get; set; }
+    }
+}
